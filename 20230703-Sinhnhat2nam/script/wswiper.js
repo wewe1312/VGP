3007bb913c9599d152c46f5d7851ce1680e569da
Wewe <wewe1312@gmail.com>
2023-07-14 11:04:21 +0700

    Update

--- 20230703-Sinhnhat2nam/script/wswiper.js
+++ 20230703-Sinhnhat2nam/script/wswiper.js
@@ -1,68 +1,17 @@
-import Swiper from "https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.esm.browser.min.js";
+import Swiper from "https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.mjs";
 
-const swiper = new Swiper(".character-slide", {
+const swiper = new Swiper(".shop-swiper", {
   slidesPerView: 1,
-  autoplay: {
-    delay: 5000,
-  },
-  loop: true,
-  spaceBetween: 10,
-  watchSlidesProgress: true,
-  pagination: {
-    el: ".char-pagination",
-    clickable: true,
-    currentClass: "active",
-    renderBullet: function (index, className) {
-      return (
-        '<span class="aspect-square ' +
-        className +
-        '">' +
-        (index + 1) +
-        "</span>"
-      );
-    },
-  },
-  effect: "creative",
-  creativeEffect: {
-    prev: {
-      // will set `translateZ(-400px)` on previous slides
-      translate: ["-50%", 0, -100],
-      scale: 0.69,
-      opacity: 0,
-    },
-    next: {
-      // will set `translateX(100%)` on next slides
-      translate: ["50%", 0, -100],
-      scale: 0.69,
-      opacity: 0,
-    },
-  },
-});
-
-const swiper2 = new Swiper(".feature-slide", {
-  slidesPerView: 1,
-  centeredSlides: true,
-  autoplay: {
-    delay: 5000,
-  },
-  loop: true,
-  effect: "creative",
-  creativeEffect: {
-    prev: {
-      // will set `translateZ(-400px)` on previous slides
-      translate: ["-50%", 0, -100],
-      scale: 0.69,
-      opacity: 0,
-    },
-    next: {
-      // will set `translateX(100%)` on next slides
-      translate: ["50%", 0, -100],
-      scale: 0.69,
-      opacity: 0,
-    },
-  },
+  loop: false,
   navigation: {
     nextEl: ".button-next",
     prevEl: ".button-prev",
+  },
+  pagination: {
+    el: ".swiper-pag",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
+    },
   },
 });
