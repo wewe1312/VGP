c83a53827f6216db02f47896230d3209a9f303ef
Wewe <wewe1312@gmail.com>
2024-01-30 15:31:05 +0700

    Upload

---
+++ Hompage/script/wswiper.js
@@ -0,0 +1,65 @@
+import Swiper from "https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.esm.browser.min.js";
+
+const swiper = new Swiper(".newsboard", {
+  slidesPerView: 1,
+  autoplay: {
+    delay: 5000,
+  },
+  loop: true,
+  spaceBetween: 0,
+  watchSlidesProgress: true,
+});
+
+const swiperThumb = new Swiper(".character-thumb-slide", {
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+const swiper2 = new Swiper(".character-slide", {
+  slidesPerView: 1,
+  centeredSlides: true,
+  effect: "creative",
+  creativeEffect: {
+    prev: {
+      translate: ["-10%", 0, "10%"],
+      opacity: 0,
+    },
+    next: {
+      translate: ["10%", 0, 0],
+      opacity: 0,
+    },
+  },
+  autoplay: {
+    delay: 5000,
+  },
+  loop: true,
+  thumbs: {
+    swiper: swiperThumb,
+  },
+});
+
+const swiper3 = new Swiper(".feature-slide", {
+  slidesPerView: 1.6,
+  centeredSlides: true,
+  effect: "creative",
+  creativeEffect: {
+    prev: {
+      translate: ["-40%", 0, 0],
+      scale: 0.8,
+      opacity: 1,
+    },
+    next: {
+      translate: ["40%", 0, 0],
+      scale: 0.8,
+      opacity: 1,
+    },
+  },
+  autoplay: {
+    delay: 5000,
+  },
+  loop: true,
+  navigation: {
+    nextEl: ".button-next",
+    prevEl: ".button-prev",
+  },
+});
