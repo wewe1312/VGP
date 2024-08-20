fe0d5b57f95263c01d2c2567625fb92d1bf6eac8
Wewe <wewe1312@gmail.com>
2024-08-20 15:52:06 +0700

    Upload

---
+++ Landing Ra Mat/script/wswiper.js
@@ -0,0 +1,51 @@
+import Swiper from "https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.esm.browser.min.js";
+
+const swiper = new Swiper(".video-slide", {
+  slidesPerView: 1,
+  autoplay: {
+    delay: 5000,
+  },
+  loop: true,
+  spaceBetween: 10,
+  watchSlidesProgress: true,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  breakpoints: {
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+const swiper2 = new Swiper(".feature-slide", {
+  slidesPerView: 1,
+  centeredSlides: true,
+  autoplay: {
+    delay: 5000,
+  },
+  loop: true,
+  effect: "creative",
+  creativeEffect: {
+    prev: {
+      // will set `translateZ(-400px)` on previous slides
+      translate: ["-50%", 0, -100],
+      scale: 0.69,
+      opacity: 0,
+    },
+    next: {
+      // will set `translateX(100%)` on next slides
+      translate: ["50%", 0, -100],
+      scale: 0.69,
+      opacity: 0,
+    },
+  },
+  navigation: {
+    nextEl: ".btn-next",
+    prevEl: ".btn-prev",
+  },
+  pagination: {
+    el: ".feature-pagination",
+  },
+});
